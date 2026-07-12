d0795138a2cd8e0c980891ff8c0d4aa273d45776
sayan5126 <sayandan57@gmail.com>
2026-07-12 13:25:07 +0530

    Implement form submission logic and state handling in TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { useTodoContext } from "../context";
 
 export default function TodoForm() {
-    
+    const [todoMsg , setTodoMsg] = useState("")
+    const {addTodo} = useTodoContext()
+    const add = (e) => {
+        e.preventDefault()
+        if(!todoMsg) return
+        addTodo({todoMsg : todoMsg , completed : false})
+        setTodoMsg("");
+    }
 
     return (
-        <form  className="flex">
+        <form onSubmit={add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
+                value={todoMsg}
+                onChange={(e)=>setTodoMsg(e.target.value)}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
